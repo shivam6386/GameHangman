78b01b6a6523fa7324ac4bc0bf91c9732a954125
shivam <shivammanitripathishiv@gmail.com>
2024-12-12 16:27:29 +0530

    useLocation

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,6 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 function PlayGame(){
+  const {state}=useLocation();
     return(
-      <div>Playgame</div>
+      <div>Playgame {state.wordSelected}</div>
     )
 }
 
