9c75f8e40cd09174bd7d7d7152fe0c9a4d0b06ce
shivam <shivammanitripathishiv@gmail.com>
2024-12-13 15:20:14 +0530

    Added Masked and Letter Button

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,13 +1,24 @@
 import { useLocation } from "react-router-dom";
+import Maskedtext from "../components/MaskedText/MaskedText";
+
+import LetterButtons from "../components/LetterButtons/LetterButtons";
+ import { useState } from "react";
 
 function PlayGame(){
 
   const {state}=useLocation();
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
-
+  function handleLetterClick(letter){
+    setGuessedLetters([...guessedLetters, letter]);
+  }
   
-    return(
-      <div>Playgame {state.wordSelected}</div>
+    return(<>
+   
+    <Maskedtext text={state.wordSelected} guessedLetters={guessedLetters} />
+    <LetterButtons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={handleLetterClick} />
+    </>
+      
     )
 }
 
